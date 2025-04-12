8470f82010ca4abd90808da58ae116f55061c53f
fernandosalomon <fernandosalomon1992@gmail.com>
2025-04-12 15:35:45 -0300

    fix error in openPlaces.sh and openApps.sh scripts

--- config/eww/scripts/openApps.sh
+++ config/eww/scripts/openApps.sh
@@ -3,28 +3,28 @@
 EWW=`which eww`
 
 if [[ "$1" == "--firefox" ]]; then
-	${EWW} kill && ${EWW} close-all && firefox &
+	$EWW close-all && firefox &
 
 elif [[ "$1" == "--spotify" ]]; then
-	${EWW} kill && ${EWW} close-all && spotify &
+	$EWW close-all && spotify &
 
 elif [[ "$1" == "--terminal" ]]; then
-	${EWW} kill && ${EWW} close-all && kitty ~ &
+	$EWW close-all && kitty ~ &
 
 elif [[ "$1" == "--files" ]]; then
-	${EWW} kill && ${EWW} close-all && thunar ~ &
+	$EWW close-all && thunar ~ &
 
 elif [[ "$1" == "--code" ]]; then
-	${EWW} kill && ${EWW} close-all && code ~ &
+	$EWW close-all && code ~ &
 
 elif [[ "$1" == "--screenshot" ]]; then
-	${EWW} kill && ${EWW} close-all &&  grim -g "$(slurp)" &
+	$EWW close-all &&  grim -g "$(slurp)" &
 
 elif [[ "$1" == "--firefox-priv" ]]; then
-	${EWW} kill && ${EWW} close-all && firefox --private-window &
+	$EWW close-all && firefox --private-window &
 
 elif [[ "$1" == "--search" ]]; then
-	${EWW} kill && ${EWW} close-all && firefox --new-window https://chatgpt.com/ &
+	$EWW close-all && firefox --new-window https://chatgpt.com/ &
 
 else
     echo "Wrong option"
--- config/eww/scripts/openPlaces.sh
+++ config/eww/scripts/openPlaces.sh
@@ -3,22 +3,22 @@
 EWW=`which eww`
 
 if [[ "$1" == "--Documents" ]]; then
-	${EWW} kill && ${EWW} close-all && thunar $HOME/Documentos &
+	$EWW close-all && thunar $HOME/Documentos &
 
 elif [[ "$1" == "--Downloads" ]]; then
-	${EWW} kill && ${EWW} close-all && thunar $HOME/Descargas &
+	$EWW close-all && thunar $HOME/Descargas &
 
 elif [[ "$1" == "--Pictures" ]]; then
-	${EWW} kill && ${EWW} close-all && thunar $HOME/Pictures &
+	$EWW close-all && thunar $HOME/Pictures &
 
 elif [[ "$1" == "--config" ]]; then
-	${EWW} kill && ${EWW} close-all && thunar $HOME/.config &
+	$EWW close-all && thunar $HOME/.config &
 
 elif [[ "$1" == "--ProgramFiles" ]]; then
-	${EWW} kill && ${EWW} close-all && thunar $HOME/ProgramFiles &
+	$EWW close-all && thunar $HOME/ProgramFiles &
 
 elif [[ "$1" == "--Imagenes" ]]; then
-	${EWW} kill && ${EWW} close-all && thunar $HOME/Imágenes &
+	$EWW close-all && thunar $HOME/Imágenes &
 
 else
     echo "Wrong option"
