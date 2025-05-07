22fd708a18b5e7b097dbb5bbd8c886f1786e7314
fernandosalomon <fernandosalomon1992@gmail.com>
2025-05-07 15:24:34 -0300

    fix error when creating wallpapers symlink

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@ ln -s $HOME/.dotfiles/eww $HOME/.config/eww
 ln -s $HOME/.dotfiles/fastfetch $HOME/.config/fastfetch
 ln -s $HOME/.dotfiles/nvim $HOME/.config/nvim
 ln -s $HOME/.dotfiles/rofi $HOME/.config/rofi
-ln -s $HOME/.dotfiles/wallpapers $HOME/.config/hypr/wallpapers
 ln -s $HOME/.dotfiles/waybar $HOME/.config/waybar
 ln -s $HOME/.dotfiles/hypr $HOME/.config/hypr
+ln -s $HOME/.dotfiles/wallpapers $HOME/.config/hypr/wallpapers
 ln -s $HOME/.dotfiles/kitty $HOME/.config/kitty
 ln -s $HOME/.dotfiles/swaync $HOME/.config/swaync
 ln -s $HOME/.dotfiles/zsh $HOME/.config/zsh
